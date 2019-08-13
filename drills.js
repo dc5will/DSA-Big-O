742ad098073fc29100bdff47cab06362f3b8d157
William Wong <wwong1987@gmail.com>
2019-08-13 11:25:20 -0700

    finished up to What Am I problem

--- drills.js
+++ drills.js
@@ -62,6 +62,107 @@ function doubleArrayValues(array) {
 
 // O(n) = linear because the operation is dependent on size of array. Directly proportional to input size of array
 
+// =================== Naive search =====================
+// TODO: What is the Big O of the following algorithm? Explain your answer
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+
+// O(n) = linear because the operation is dependent on size of the array which is directly proportional to input size of the array.
+
+// ==================== Creating pairs ===================
+// TODO: What is the Big O of the following algorithm? Explain your answer
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+// O(n^2) = Polynomial. Nested for loop. Two levels of for loops.
+
+// ===================== Compute the sequence ==================
+// TODO: What does the following algorithm do? What is its runtime complexity? Explain your answer
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+
+// Adds the first 2 indexes of the array and stores it in a different array.
+// It is O(1) - constant because it is appending at the end of the array due to reassigning indices.
+
+// ====================== An efficient search ====================
+// TODO: In this example, we return to the problem of searching using a more sophisticated approach than in naive search, above. Assume that the input array is always sorted. What is the Big O of the following algorithm? Explain your answer.
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+// O(log(n)) = logarithmic time - always cuts the amount of items it needs to search in half (divide and conquer). The time complexity increases slowly in a logarithmic fashion.
+
+// ==================== Random element ======================
+// TODO: What is the Big O of the following algorithm? Explain your answer
+function findRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// O(1) = constant time - simple calculation. Doesn't depend on the length of the array. No iterations occuring.
+
+
+// ==================== What Am I? =============================
+// TODO: What does the following algorithm do? What is the Big O of the following algorithm? Explain your answer
+function isWhat(n) {
+    if (n < 2 || n % 1 != 0) {
+        return false;
+    }
+    for (let i = 2; i < n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+// The first conditional checks if inputed number is less than 2 or if its a whole number. The for loop checks to see if the inputed number is a prime number.
+// O(n) - Linear run time complexity. The for loop that iterates through each item once and only once.
+
+
+
+
+
+
 
 
 
